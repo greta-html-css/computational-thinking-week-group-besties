a788dce2be513c47011ee61af31e2cdbd79509c9
anitawang09 <anita930929@gmail.com>
2025-09-02 14:11:00 +0200

    Update station6.py

--- challenge_day2/station6.py
+++ challenge_day2/station6.py
@@ -6,16 +6,15 @@ return math.sin(x)
 # Given observations:
 observations = [(0.3, 0.2955, 21),
 # sample_in, sample_out, input10
-(1.4, 0.9854, 66),11
-(1.6, 0.9996, 12),
+  (1.4, 0.9854, 66),11
+  (1.6, 0.9996, 12),
+]
 
-
-if _name_
-= "_main_":
-for sample_in, sample_out, real_input in observations:
-expected _sample = station_output (sample_in)
-real_output = station6_output(real_input)
-print(f"Sample In: {sample_in} | Sample Out:
-{sample_out} (calc (expected_sample: .4f})")
-print(f"Input: (real_input} | Output: {real_output:.4f}")
-print("-" * 50)
+if _name_== "_main_":
+    for sample_in, sample_out, real_input in observations:
+      expected _sample = station_output (sample_in)
+      real_output = station6_output(real_input)
+      print(f"Sample In: {sample_in} | Sample Out:
+      {sample_out} (calc (expected_sample: .4f})")
+      print(f"Input: (real_input} | Output: {real_output:.4f}")
+      print("-" * 50)
