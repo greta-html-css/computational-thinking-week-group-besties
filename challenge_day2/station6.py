913dfe41b34a6760bfcbbdbff3383f9b9b932630
anitawang09 <anita930929@gmail.com>
2025-09-02 13:25:34 +0200

    Update station6.py

--- challenge_day2/station6.py
+++ challenge_day2/station6.py
@@ -1,2 +1,32 @@
-def solution_station_6(n):
-    if 
+from tests import tests
+
+def solution_station_6(numbers):
+    def sin_taylor(x):
+        pi = 3.141592653589793
+        while x > 2 * pi:
+            x -= 2 * pi
+        while x < -2 * pi:
+            x += 2 * pi
+        
+        result = 0
+        term = x
+        n = 1
+        
+        for i in range(15):
+            result += term
+            term *= -x * x / ((n + 1) * (n + 2))
+            n += 2
+        
+        return result
+    
+    results = []
+    for num in numbers:
+        results.append(sin_taylor(num))
+    return results
+
+#sample input, sample output, input
+#2.2, 52, 0.8085
+#0.3, 21, 0.2955
+#1.4, 66, 0.9854
+#1.6, 12, 0.996
+#0.7, 45, 0.6442
