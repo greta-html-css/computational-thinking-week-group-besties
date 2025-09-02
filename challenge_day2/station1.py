a91a9f128b951449da3f7a1a849cb4d37099d96b
jacekkalten <jacekkalten@gmail.com>
2025-09-02 09:37:19 +0200

    Update station1.py

--- challenge_day2/station1.py
+++ challenge_day2/station1.py
@@ -1,2 +1,11 @@
 from tests import tests
 from station1 import solution_station_1
+#sample input, intput, sample output, output
+#96, 9, 1680708854858330000, ?
+#20,79,6765, ?
+#62, 57, 4052739537881, ?
+#68,53,72723460248141,?
+#16,77,987,?
+#38,25,39088169,?
+#54,77,86267571272,?
+#Fibinachi sequence 
