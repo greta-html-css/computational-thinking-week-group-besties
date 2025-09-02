bc78750e28a65d3ceae85d80dbf25973708f00e5
greta-html-css <greta.pfeiffer@student.uva.nl>
2025-09-02 13:04:39 +0200

    Added way of converting date to day of the week station2.py

--- challenge_day2/station2.py
+++ challenge_day2/station2.py
@@ -1,4 +1,6 @@
-# Station 2 - Dates and Days of the Week (in Japanese Kanji)
+# Station 2 Dates and Days of the Week (in Japanese Kanji)
+
+# Observations
 # 09:40:20, 2023-09-25, "月曜日 (Monday)", 2024-06-20
 # 09:42:17, 2023-06-10, "土曜E (Saturday)", 2024-04-21
 # 09:42:21, 2023-02-09, "木曜日 (Thursday)", 2024-11-03
@@ -9,6 +11,25 @@
 # 09:43:21, 2023-10-11, "水曜日 (Wednesday)", 2024-08-11
 # 09:43:32, 2023-11-07, "火曜日 (Tuesday)", 2024-06-09
 # 09:43:44, 2023-07-24,"月曜日 (Monday)", 2024-06-25
+
+from tests import tests
+from station2 import solution_station_2
+
+# Using Zeller’s Congruence to figure out the day of the week
+d = input_date
+q, m, y = map(int, d.split())  # day, month, year
+
+if m < 3:   
+    m += 12
+    y -= 1
+
+k = y % 100  # yearpart
+j = y // 100 # century
+
+h = (q + (13 * (m + 1)) // 5 + k + k // 4 + j // 4 + 5 * j) % 7  
+wd = ['土曜E (Saturday)', '日曜日 (Sunday)', '月曜日 (Monday)', '火曜日 (Tuesday)', '水曜日 (Wednesday)', '木曜日 (Thursday)', '金曜且 (Friday)']
+
+print(wd[h])
 
 # Station 5 - Simultanious Observstions
 # 10:23:00, "Yang Yang", 4, "Madeleine"
