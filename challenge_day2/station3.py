88b70ed50a4d50e299d2e5d2b2387f49377e959c
İpek Öner <ipekoner2006@gmail.com>
2025-09-02 09:40:33 +0200

    station 3

--- challenge_day2/station3.py
+++ challenge_day2/station3.py
@@ -1,1 +1,11 @@
+#sample input, intput, sample output, output
+# 48, True, 71122, ...
+#6, True, 46388, ...
+#76 False 24854
+#76 False 52065
+#12 True 21486
+#87 True 60795
+#42 True 12439
+#21 True 67847
+#53 False 22212
 
