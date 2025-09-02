f870b450f518596bd0e4f3404d187fbbc651f88a
İpek Öner <ipekoner2006@gmail.com>
2025-09-02 13:20:52 +0200

    lets hope

--- challenge_day2/station5.py
+++ challenge_day2/station5.py
@@ -19,3 +19,22 @@
 #Oleysa, 6, Nadee
 #Greta, 5, Alissa 
 
+def solution_station_5(n):
+    learning_teams = {
+        "1": ["Daeho, David, Kaisa, Oliver, Sara, Dan, Ivar, Lotte, Riya, Vassil, Twan, Ester, Karolina, Lena, Margarita, Anna, Kien, Klaudia, Maliah, Todd"],
+        "2": ["Oumaima, Mathilde, Marie, Anita, Ziyan, Bernardo, Eleanor, Lorijn, Maria, Younes, Yvan, Henning, Liangyu, Maciej, Toprak, Chris, GengXin, Mingze, Phoebe"],
+        "3": ["Betija, Haider, Kacper, Sophie, Amir, Baltasar, Isar, Jelle, Nicolas, David, Ipek, Juan, Marfa, Maria, Alissa, Leopoldo, Mies, Jiaying, Kaixin, Mai, Sem, Tibbe"],
+        "4": ["Justus, Julia, Philip, Uli, Vanessa, Anna, Ekaterina, Thessa, Tongfei, Yang, Benedikt, Jan, Nadee, Osjah, Tim, Eliana, Joana, Peilin, Pija, Wenhao"],
+        "5": ["Afua, Cristina, Greta, Jace, Laura, Anna, Bassant, Ivan, Juriaan, Kiavash"],
+        "6": ["Keitaro, Nohemi, Norina, Yifan, Yinan, Luo, Nikola, Olesya, Sophie, Tom"]
+
+    }
+
+    name_to_teams = {}
+    for team, names_list in learning_teams.items():
+        for names_str in names_list:
+            for name in [n.strip() for n in names_str.split(",")]:
+                name_to_teams[name] = int(team)
+            
+    return name_to_teams.get(n, -1)
+
