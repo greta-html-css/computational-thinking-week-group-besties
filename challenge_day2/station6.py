da30a383ed81dfa05ef142e45d39bab6762df9fd
greta-html-css <greta.pfeiffer@student.uva.nl>
2025-09-02 14:00:28 +0200

    Trying something station6.py

--- challenge_day2/station6.py
+++ challenge_day2/station6.py
@@ -1,22 +1,39 @@
 from tests import tests
 
 def solution_station_6(numbers):
+    # def sin_taylor(x):
+        # pi = 3.141592653589793
+        # while x > 2 * pi:
+            # x -= 2 * pi
+        # while x < -2 * pi:
+            # x += 2 * pi
+        
+        # result = 0
+        # term = x
+        # n = 1
+        # for i in range(15):
+            # result += term
+            # term *= -x * x / (n * (n + 1))
+            # n += 2
+        
+        # return result
+
     def sin_taylor(x):
         pi = 3.141592653589793
         while x > 2 * pi:
             x -= 2 * pi
         while x < -2 * pi:
             x += 2 * pi
-        
+
         result = 0
         term = x
-        n = 1
-        for i in range(15):
+    
+        for i in range(1, 16): 
             result += term
-            term *= -x * x / (n * (n + 1))
-            n += 2
+            term *= -x * x / ((2 * i) * (2 * i + 1))
         
         return result
+
     
     results = []
     for num in numbers:
