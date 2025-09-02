c5f132e93875474091dfd998ef2c2d765a729674
bhassant202 <bassant.hassan@student.uva.nl>
2025-09-02 13:19:29 +0200

    Update station7.py

--- challenge_day2/station7.py
+++ challenge_day2/station7.py
@@ -1,6 +1,7 @@
-def solution_station_7(n):
+def solution_station_7(equation):
     a = 3
     b = -1
     c = 4
     d = 7
     e = 0.5
+    return eval(equation, {"a": a, "b": b, "c": c, "d": d, "e": e})
