30f5ad7a97c830c26d88f5a0e41533b686f5c8bf
Osjah-Kazmi <osjahrazakazmi@gmail.com>
2025-09-04 15:49:09 +0200

    fixed issue in sort.jl

--- challenge_day4/Julia_step3/sort.jl
+++ challenge_day4/Julia_step3/sort.jl
@@ -1,7 +1,7 @@
 using DataFrames, CSV, Statistics, DelimitedFiles
 
 # Read the CSV file into a DataFrame
-people_df = CSV.File("data3.csv") |> DataFrame
+people_df = CSV.File("C:\Users\osjah\OneDrive\Documents\GitHub\computational-thinking-week-group-besties\computational-thinking-week-group-besties\challenge_day4\Go_step2\data3.csv") |> DataFrame
 
 # Function to classify a score based on quartiles
 function classify_score(score, quartiles)
